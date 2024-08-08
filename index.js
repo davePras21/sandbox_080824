1c59a468ab4feaea3df4dada3cd28aca27033dfe
davePras21 <dave@feedloop.ai>
2024-08-08 14:45:59 +0700

    change endpoint

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 // app.use(morgan('dev'));
 
 // Define Routes
-app.get('/api', async (req, res) => {
+app.get('/api/norek', async (req, res) => {
   const datas = await controller()
   res.send(datas);
 });
